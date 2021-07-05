b1e725412fc2f3f29aff41cf01939afa5cddf526
Ivan Zhevakin <ixvakin@gmail.com>
2021-07-05 16:47:34 +0300

    update main page

--- pages/index.vue
+++ pages/index.vue
@@ -30,19 +30,21 @@
           Linked In
         </a>
       </div>
-      <div class="mb-4">
+      <h4 class="font-bold mb-2">About</h4>
+      <p class="mb-8">
         Working as a web developer since 2008. Currently I’m focusing on
         building the complex web applications using JS frameworks: React, Vue,
         Angular. Besides I really care about good UI/UX and always trying not
         just build the application but deliver great experience to users. Also I
         believe that great projects could be completed only by high motivated
         teams so I’m always trying to keep team communication at a high level.
-      </div>
+      </p>
       <div>
+        <h4 class="font-bold mb-2">Job experience</h4>
         <article
           v-for="(job, index) in experience.jobs"
           :key="index"
-          class="mb-12"
+          class="mb-8"
         >
           <small class="gray-300 text-sm">
             <span v-if="job.till">
