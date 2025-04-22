9a4482ecfdf38ee53a68d2b9cfac91374b022329
Ivan Zhevakin <ixvakin@gmail.com>
2025-04-22 12:39:08 +0200

    fix

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import { NextIntlClientProvider, useMessages } from 'next-intl';
+import { NextIntlClientProvider } from 'next-intl';
 import '../globals.css';
 import LanguageSwitcher from '@/components/LanguageSwitcher';
 
