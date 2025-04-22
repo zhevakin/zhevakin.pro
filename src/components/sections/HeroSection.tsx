582350b966fd1671dbb147b35ef9759e8c6d4472
Ivan Zhevakin <ixvakin@gmail.com>
2025-04-22 13:32:19 +0200

    ашч

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { useTranslations } from 'next-intl';
+import { LuMapPin } from 'react-icons/lu';
 
 export default function HeroSection() {
   const t = useTranslations();
@@ -17,10 +18,14 @@ export default function HeroSection() {
           />
         </div>
         <div>
-          <h1 className="mb-4 text-4xl font-bold text-gray-900 dark:text-white">
+          <h1 className="mb-2 text-4xl font-bold text-gray-900 dark:text-white">
             {t('hero.title')}
           </h1>
-          <p className="text-xl text-gray-600 dark:text-gray-300">{t('hero.subtitle')}</p>
+          <p className="mb-2 text-xl text-gray-600 dark:text-gray-300">{t('hero.subtitle')}</p>
+          <div className="flex items-center gap-2">
+            <LuMapPin className="text-gray-600 dark:text-gray-300" />
+            <p className="text-gray-600 dark:text-gray-300">{t('hero.location')}</p>
+          </div>
         </div>
       </div>
     </section>
